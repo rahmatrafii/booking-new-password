da901f419bc00859cc77d1a67a5ad392b1c3b493
Rahmat Rafi Indrayani <rahmatrafiindrayani@gmail.com>
2026-03-05 20:59:47 +0700

    remove route wildcard and navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ResetPassword from "./pages/Reset-password";
 
 function App() {
